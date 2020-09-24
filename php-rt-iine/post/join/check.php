9fa44b2c3a4c7631cc117b9febaa2c93e28e2475
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-09-24 17:49:32 +0900

    テキスト通りのファイル

---
+++ php-rt-iine/post/join/check.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require('../dbconnect.php');
+
+if(!isset($_SESSION['join'])){
+    header('Location: index.php');
+    exit();
+}
+
+if(!empty($_POST)){
+    $statement = $db->prepare('INSERT INTO members SET name=?,email=?,password=?,picture=?,created=NOW()');
+    echo $ret = $statement->execute(array(
+        $_SESSION['join']['name'],
+        $_SESSION['join']['email'],
+        sha1($_SESSION['join']['password']),
+        $_SESSION['join']['image']
+    ));
+    unset($_SESSION['join']);
+
+    header('Location: thanks.php');
+    exit();
+}
+?>
+
+<form action="" method="post">
+    <input type="hidden" name="action" value="submit" />
+    <dl>
+    <dt>ニックネーム</dt>
+        <dd>
+            <?php echo htmlspecialchars($_SESSION['join']['name'],ENT_QUOTES); ?>
+        </dd>
+        <dt>メールアドレス</dt>
+        <dd>
+            <?php echo htmlspecialchars($_SESSION['join']['email'],ENT_QUOTES); ?>
+        </dd>
+        <dt>パスワード</dt>
+        <dd>
+            【表示されません】
+        </dd>
+        <dt>写真など</dt>
+        <dd>
+            <img drc="../member_picture/<?php echo htmlspecialchars($_SESSION['join']['image'],ENT_QUOTES); ?>" width="100" height="100" alt="" />
+        </dd>
+    </dl>
+    <div><a href="index.php?action=rewrite">&laquo;&nbsp;</a>｜<input type="submit" value="登録する" /></div>
+</form>
