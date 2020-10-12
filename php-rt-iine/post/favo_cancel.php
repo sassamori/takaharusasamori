3a167486d6d27777461641a2250c5d850eac493b
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-10-12 18:23:54 +0900

    favo_cancel.phpでいいねした当人しかキャンセルできない仕様に修正

--- php-rt-iine/post/favo_cancel.php
+++ php-rt-iine/post/favo_cancel.php
@@ -3,9 +3,13 @@
 session_start();
 require_once('dbconnect.php');
 
-if(isset($_GET['id']) && isset($_SESSION['id'])){
-    $favo_cancel = $db->prepare('UPDATE favos SET delete_flag=? WHERE post_id=?');
-    $favo_cancel->execute(array(1,$_GET['id']));
+$favo_records = $db->prepare('SELECT * FROM favos WHERE delete_flag=0 AND post_id=?');
+$favo_records->execute(array($_GET['id']));
+$favo_record = $favo_records->fetch();
+
+if(isset($_GET['id']) && $_SESSION['id'] == $favo_record['pushing_member_id']){
+    $favo_cancel = $db->prepare('UPDATE favos SET delete_flag=1 WHERE post_id=?');
+    $favo_cancel->execute(array($_GET['id']));
 
     header('Location: index.php');
     exit();
