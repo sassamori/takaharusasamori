e59675b2f94e21b731d75900c34c17da580dfeb0
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-10-25 22:47:12 +0900

    他人のRTまで消えてしまうバグを修正

--- php-rt-iine/post/rt_cancel.php
+++ php-rt-iine/post/rt_cancel.php
@@ -8,17 +8,11 @@ if(isset($_GET['id']) && isset($_SESSION['id'])){
     $posts = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=?');
     $posts->execute(array($_GET['id']));
     //準備
-    $relations_before = $db->query(
-        'SELECT posts.id AS important_id,posts.*,rts.* FROM posts,rts WHERE posts.rt_post_id=rts.post_id');
-    $relations_before_all = $relations_before->fetchall();
-    //リツイートのdelete_flagを1にして、リツイート情報のrt_delete_flagも1にする
-    foreach($relations_before_all as $relation_before){
-        if($_GET['id'] == $relation_before['rt_post_id'] && $_SESSION['id'] == $relation_before['rt_member_id']){
-            $relations = $db->prepare(
-                'UPDATE posts,rts SET delete_flag=1,rt_delete_flag=1 WHERE posts.rt_post_id=rts.post_id AND posts.id=?');
-            $relations->execute(array($relation_before['important_id']));
-        }
-    }
+    $relations = $db->prepare(
+        'UPDATE posts left JOIN rts ON posts.rt_post_id=rts.post_id AND posts.rt_member_id = rts.rt_member_id
+                    SET posts.delete_flag=1,rts.rt_delete_flag=1,posts.modified=now(),rts.modified=now() 
+                    WHERE posts.rt_post_id = ? AND rts.rt_member_id = ?');
+    $relations->execute([$_GET['id'],$_SESSION['id']]);
     header('Location: index.php');
     exit();
 }
